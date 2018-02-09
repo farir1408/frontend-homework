a984363cb9bb576f6b9f1041235db1fd9ffc9bb0
farir1408 <farir1881@gmail.com>
2018-02-09 12:03:14 +0300

    fixed

--- source/rle.js
+++ source/rle.js
@@ -2,8 +2,8 @@
 
 const REGEXP = /(.)\1*/g;
 
-const rle = (string) => {
-	return string.match(REGEXP).map((element) => {
-		return element[0] + (element.length > 1 ? element.length.toString() : '')
-	}).join('');
+const rle = string => {
+	return string.match(REGEXP).reduce((result, element) => {
+		return result + element[0] + (element.length > 1 ? element.length : '')
+	}, '');
 }
